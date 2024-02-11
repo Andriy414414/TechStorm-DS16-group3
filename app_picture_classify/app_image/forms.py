52bf3515bce64ae43ef6db8f4a72ad2ccda0ec53
evgen-kulik <e.kulik.atom@gmail.com>
2024-02-11 23:40:21 +0100

    Збереження картинки та її URL
    Картинка завантажується, зтискається в 32х32 пікселі, завантажується в Клаудинарі, її url зберыгаэться в БД

--- app_picture_classify/app_image/forms.py
+++ app_picture_classify/app_image/forms.py
@@ -1,10 +1,11 @@
 from django.forms import ModelForm, ImageField, FileInput
 from .models import ImageModel
 
+
 class ImageForm(ModelForm):
     path = ImageField(widget=FileInput(attrs={
-        "class": "form-control-lg form-control mx-auto", 
-        "name": "image", 
+        "class": "form-control-lg form-control mx-auto",
+        "name": "image",
         "id": "image",
         "accept": "image/*",
         }))
--- app_picture_classify/app_image/models.py
+++ app_picture_classify/app_image/models.py
@@ -1,7 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+
+class LabelsModel(models.Model):
+    label_of_class = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    names_of_class = models.CharField(max_length=100, choices=[
+        ('airplane', 'airplane'),
+        ('car', 'car'),
+        ('bird', 'bird'),
+        ('cat', 'cat'),
+        ('deer', 'deer'),
+        ('dog', 'dog'),
+        ('frog', 'frog'),
+        ('horse', 'horse'),
+        ('ship', 'ship'),
+        ('truck', 'truck'),
+    ])
 
 
 class ImageModel(models.Model):
-    path = models.ImageField(upload_to='pictures')
+    cloudinary_image = CloudinaryField('image')  # збереження зображення в хмарі, а його url в БД
+    array = ArrayField(models.IntegerField(), blank=True, null=True, default=list)  # поле для збереження масиву пікселів
+    # blank=True та null=True вказують, що поле може бути порожнім, і default=list встановлює порожній список як значення за замовчуванням
+    label_of_class = models.ForeignKey(LabelsModel, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
