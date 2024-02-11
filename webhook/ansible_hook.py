34afea1232ee72948aaff536231c1ff62210102c
dCrowb <oishevchenko94@gmail.com>
2024-02-11 19:17:43 +0200

    fix deploy error

--- webhook/ansible_hook.py
+++ webhook/ansible_hook.py
@@ -6,7 +6,7 @@ HTTP_METHODS = ['GET', 'POST']
 app = Flask(__name__)
 
 def run_command():
-    return subprocess.Popen("echo 'ansible-playbook ~/Picture_Classify_Hub/ansible/playfile/main.yml' > /usr/pipe", shell=True, stdout=subprocess.PIPE).stdout.read()
+    return subprocess.Popen("echo 'cd ~/Picture_Classify_Hub && ansible-playbook ./ansible/playfile/main.yml' > /usr/pipe", shell=True, stdout=subprocess.PIPE).stdout.read()
 
 @app.route('/', methods=['GET'])
 def index():
