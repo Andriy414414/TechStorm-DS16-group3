a8f65597e6bd3d9fae4181a8cfa14d2558f53472
Evgenius87 <plotnikov.evgenij@ukr.net>
2024-02-11 18:56:22 +0200

    create image validatoin

--- app_picture_classify/app_image/models.py
+++ app_picture_classify/app_image/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from .image_validations import validate_image_format, validate_image_size
 
-# Create your models here.
+
 
 
 class ImageModel(models.Model):
-    path = models.ImageField(upload_to='pictures')
+    path = models.ImageField(upload_to='pictures', validators=[validate_image_size, validate_image_format])
