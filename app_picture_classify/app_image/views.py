17f8dcf21ce65d0ea5ee8f7672047eb57ff17845
evgen-kulik <e.kulik.atom@gmail.com>
2024-02-12 22:33:13 +0100

    Update views.py
    Підготовка до оновлення гілки

--- app_picture_classify/app_image/views.py
+++ app_picture_classify/app_image/views.py
@@ -1,19 +1,45 @@
+from .utils import cutting_image, save_picture_to_claud, save_image_url_to_db
+from django.apps import apps
 from django.shortcuts import render, redirect
-
 from .forms import ImageForm
 from .models import ImageModel
+from .image_handler import preprocess_image
 
-from .utils import cutting_image, save_picture_to_claud, save_image_url_to_db
+
+class ModelInference:
+    def __init__(self, model):
+        self.model = model
+
+    def predict_class(self, img):
+        сlass_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        prediction = self.model.predict(img)
+        predicted_class = prediction.argmax()
+
+        return сlass_names[predicted_class]
 
 
 def home(request):
-    form = ImageForm()
+    form = ImageForm(instance=ImageModel())
+    predicted_class = ''
+
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES, instance=ImageModel())
         if form.is_valid():
             uploaded_image = request.FILES['path']  # отримаємо завантажену картинку
 
-            # обробляємо картинку в 32х32 пікселя
+            # перетворення картинки в масив
+            img_32x32_array = preprocess_image(uploaded_image)
+
+            # отримання конфігурації додатка 'app_image'
+            AppConfig = apps.get_app_config('app_image')
+            # з отриманої конфігурації отримується модель
+            model = AppConfig.model
+
+            # передбачення класу зображення за допомогою переданої моделі
+            model_inference = ModelInference(model)
+            predicted_class = model_inference.predict_class(img_32x32_array)
+
+            # перетворюємо зображення в 32х32 пікселя
             img_32x32 = cutting_image(uploaded_image)
 
             # збереження зображення в хмару, отримання його url
@@ -22,9 +48,9 @@ def home(request):
             # зберігаємо URL в базі даних
             save_image_url_to_db(form, cloudinary_url)
 
-
-    return render(request, 'app_image/index.html', {'form': form})
-
+    return render(request,
+                  template_name='app_image/index.html',
+                  context={"form": form, "output_text": predicted_class})
 
 
 def action(request):
