ef1333dac9000bc68d1e89f393457a26716d8cfc
alex <asmodians@hotmail.com>
2024-02-12 16:26:33 +0200

    add_ml_model

--- app_picture_classify/app_image/views.py
+++ app_picture_classify/app_image/views.py
@@ -1,3 +1,4 @@
+from django.apps import apps
 from django.shortcuts import render, redirect
 
 from .forms import ImageForm
@@ -5,22 +6,38 @@ from .models import ImageModel
 from .image_handler import preprocess_image
 
 
-# Create your views here.
-form = ''
+class ModelInference:
+    def __init__(self, model):
+        self.model = model
+
+    def predict_class(self, img):
+        сlass_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        prediction = self.model.predict(img)
+        predicted_class = prediction.argmax()
+
+        return сlass_names[predicted_class]
+
 
 def home(request):
     form = ImageForm(instance=ImageModel())
+    predicted_class = ''
+
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES, instance=ImageModel())
         if form.is_valid():
             image_instance = form.save(commit=False)  # Отримуємо екземпляр моделі без збереження в базу даних
             uploaded_image = request.FILES['path']  # Отримуємо завантажений файл зображення
-            img_32x32 = preprocess_image(uploaded_image) # Обрізаємо зображення і зменшуємо до 32х32
-            img_32x32.save('test.jpg')
+            img_32x32_array = preprocess_image(uploaded_image) # Обрізаємо зображення і зменшуємо до 32х32
+
+            AppConfig = apps.get_app_config('app_image')
+            model = AppConfig.model
+            
+            model_inference = ModelInference(model)
+            predicted_class = model_inference.predict_class(img_32x32_array)
 
     return render(request, 
                   template_name='app_image/index.html', 
-                  context={"form": form})
+                  context={"form": form, "output_text": predicted_class})
 
 
 def action(request):
