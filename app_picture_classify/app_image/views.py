3ea515c1d3020886a8f0c4fdbe17dae033fcd135
evgen-kulik <e.kulik.atom@gmail.com>
2024-02-12 21:19:23 +0100

    Винесення функціоналу в окремі ф-ї
    Cтворення і наповнення utils.py. Після оновлення гілки продовжу.

--- app_picture_classify/app_image/views.py
+++ app_picture_classify/app_image/views.py
@@ -1,62 +1,9 @@
-import os
-import uuid
-
-from django.http import HttpResponseServerError
 from django.shortcuts import render, redirect
-from django.core.files.uploadedfile import SimpleUploadedFile
 
 from .forms import ImageForm
 from .models import ImageModel
-from .image_handler import preprocess_image
 
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
-import numpy as np
-import json
-
-
-# def home(request):
-#     form = ImageForm()
-#     if request.method == 'POST':
-#         form = ImageForm(request.POST, request.FILES, instance=ImageModel())
-#         if form.is_valid():
-#             uploaded_image = request.FILES['path']  # отримаємо завантажену картинку
-#             img_32x32 = preprocess_image(uploaded_image)  # обробляємо картинку в 32х32 пікселя
-#
-#             random_filename = str(uuid.uuid4())[:10]  # генеруємо випадкове ім'я файлу
-#             # зберігаємо оброблену картинку у тимчасовий файл
-#             temp_filename = random_filename + '.jpg'
-#             img_32x32.save(temp_filename)
-#
-#             # відкриваємо тимчасовий файл і читаємо його вміст
-#             with open(temp_filename, 'rb') as f:
-#                 img_data = f.read()
-#
-#             # створюємо об'єкт SimpleUploadedFile
-#             img_temp = SimpleUploadedFile(temp_filename, img_data, content_type='image/jpeg')
-#
-#             # видаляємо тимчасовий файл
-#             os.remove(temp_filename)
-#
-#             # завантажуємо оброблену картинку в Cloudinary
-#             uploaded_result = cloudinary.uploader.upload(img_temp, public_id=random_filename)
-#
-#             try:
-#                 cloudinary_url = uploaded_result['url']  # отримаємо URL картинки на Cloudinary
-#             except Exception as e:
-#                 return HttpResponseServerError(f"URL зображення не отримано, помилка: {str(e)}")
-#
-#             # зберігаємо URL в базі даних
-#             try:
-#                 image_instance = form.save(commit=False)
-#                 image_instance.cloudinary_image = cloudinary_url
-#                 image_instance.save()
-#             except Exception as e:
-#                 return HttpResponseServerError(f"Помилка при збереженні в БД: {str(e)}")
-#
-#     return render(request, 'app_image/index.html', {'form': form})
+from .utils import cutting_image, save_picture_to_claud, save_image_url_to_db
 
 
 def home(request):
@@ -65,39 +12,16 @@ def home(request):
         form = ImageForm(request.POST, request.FILES, instance=ImageModel())
         if form.is_valid():
             uploaded_image = request.FILES['path']  # отримаємо завантажену картинку
-            img_32x32 = preprocess_image(uploaded_image)  # обробляємо картинку в 32х32 пікселя
 
-            random_filename = str(uuid.uuid4())[:10]  # генеруємо випадкове ім'я файлу
+            # обробляємо картинку в 32х32 пікселя
+            img_32x32 = cutting_image(uploaded_image)
 
-            # зберігаємо оброблену картинку у тимчасовий файл
-            temp_filename = random_filename + '.jpg'
-            img_32x32.save(temp_filename)
-
-            # відкриваємо тимчасовий файл і читаємо його вміст
-            with open(temp_filename, 'rb') as f:
-                img_data = f.read()
-
-            # створюємо об'єкт SimpleUploadedFile
-            img_temp = SimpleUploadedFile(temp_filename, img_data, content_type='image/jpeg')
-
-            # видаляємо тимчасовий файл
-            os.remove(temp_filename)
-
-            # завантажуємо оброблену картинку в Cloudinary
-            uploaded_result = cloudinary.uploader.upload(img_temp, public_id=random_filename)
-
-            try:
-                cloudinary_url = uploaded_result['url']  # отримаємо URL картинки на Cloudinary
-            except Exception as e:
-                return HttpResponseServerError(f"URL зображення не отримано, помилка: {str(e)}")
+            # збереження зображення в хмару, отримання його url
+            cloudinary_url = save_picture_to_claud(img_32x32)
 
             # зберігаємо URL в базі даних
-            try:
-                image_instance = form.save(commit=False)
-                image_instance.cloudinary_image = cloudinary_url
-                image_instance.save()
-            except Exception as e:
-                return HttpResponseServerError(f"Помилка при збереженні в БД: {str(e)}")
+            save_image_url_to_db(form, cloudinary_url)
+
 
     return render(request, 'app_image/index.html', {'form': form})
 
