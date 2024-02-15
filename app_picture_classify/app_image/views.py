abd1174674f542f7d236cad64bcd411bcf7ec329
evgen-kulik <e.kulik.atom@gmail.com>
2024-02-15 16:32:34 +0100

    проміжний коміт, працюю над SOLID

--- app_picture_classify/app_image/views.py
+++ app_picture_classify/app_image/views.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
-from .utils import save_picture_to_claud
+from .utils import save_picture_to_claud, svg_reshape_to_32x32x3, svg_classification, save_jpeg_and_url_from_svg
 from django.http import HttpResponseServerError
 from django.apps import apps
 from django.shortcuts import render, redirect
@@ -37,12 +37,11 @@ def home(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES, instance=ImageModel())
         if form.is_valid():
-            uploaded_image = request.FILES['original_file_name']  # отримаємо завантажену картинку (тимчасовий файл)
+            uploaded_image = request.FILES['original_file_name']  # отримуємо завантажену картинку (тимчасовий файл)
+            file_extension = os.path.splitext(uploaded_image.name)[1]  # отримуємо розширення тимчасового файла
 
-            # ---------------------------------------SVG-зображення
-            file_extension = os.path.splitext(uploaded_image.name)[1]
-            print(f'РАСШИРЕНИЕ ФАЙЛА--------{file_extension}')
             if file_extension == '.svg':
+                # ---------------------------------------Векторні зображення (чорно-білі та кольорові)
 
                 # Створюємо тимчасовий файл для збереження PNG-зображення
                 with WandImage(blob=uploaded_image.read(), format='svg', width=32, height=32,
@@ -52,95 +51,78 @@ def home(request):
                         # Замінюємо вміст uploaded_image на вміст конвертованого PNG-файлу
                         uploaded_image1 = converted_img.make_blob()
 
-                # Создаем объект изображения Pillow из байтовой строки
+                # створюємо об'єкт зображення Pillow з байтового рядка
                 image = PillowImage.open(io.BytesIO(uploaded_image1))
 
-                # отримаємо масив із зображення з необхідною розмірністю (32, 32, 3)
-                img_32x32 = image.resize((32, 32))
-                image_array = np.array(img_32x32)
-                print(f'Розмірність початкового масиву растра з SVG файлу: {image_array.shape}')  # (32, 32, 4) - кольорове, (32, 32, 2) - чорно-біле
+                # # отримуємо масив із зображення з необхідною розмірністю (32, 32, 3)
+                # img_32x32 = image.resize((32, 32))
+                # image_array = np.array(img_32x32)
+                #
+                # if image_array.shape == (32, 32, 4):  # кольорове зображення
+                #     image_array = image_array[:, :, :3]  # змінюємо розмірність на (32, 32, 3)
+                # elif image_array.shape == (32, 32, 2):  # чорно-біле зображення
+                #     # копіюємо другий канал і додаємо його до image_array
+                #     third_channel = image_array[:, :, 1]  # копіюємо 2-й канал
+                #     image_array_with_third_channel = np.dstack((image_array, third_channel))  # додаємо його до image_array
+                #     image_array = image_array_with_third_channel
+                #     # створюємо об'єкт зображення Pillow із масиву image_array
+                #     img_32x32 = PillowImage.fromarray(image_array.astype('uint8'), 'RGB')
+                # else:
+                #     print(f'Розмірність масиву дорівнює {image_array.shape}, підходяча розмірність - (32, 32, 2) або (32, 32, 4)')
 
-                if image_array.shape == (32, 32, 4):  # кольорове
-                    image_array = image_array[:, :, :3]  # змінюємо розмірність на (32, 32, 3)
-                    print(f'Розмірність масиву після злиття каналів: {image_array.shape}')
-                elif image_array.shape == (32, 32, 2):  # чорно-біле
-                    # print(image_array)
-                    # копіюємо другий канал і додаємо його до image_array
-                    third_channel = image_array[:, :, 1]  # копіюємо 2й канал
-                    image_array_with_third_channel = np.dstack((image_array, third_channel))  # додаємо його до image_array
-                    image_array = image_array_with_third_channel
-                    # print(image_array)
-                    # # відображаємо зображення
-                    # plt.imshow(image_array)
-                    # plt.show()  # зображення виводиться коректно
+                # отримуємо масив із зображення з необхідною розмірністю (32, 32, 3)
+                image_array, img_32x32 = svg_reshape_to_32x32x3(image)
 
-                    # Создаем объект изображения Pillow из массива image_array
-                    img_32x32 = PillowImage.fromarray(image_array.astype('uint8'), 'RGB')
+                # # Класифікація
+                # image_array = image_array / 255.0  # нормалізація даних
+                # img_32x32_array = np.expand_dims(image_array, axis=0)  # перетворення (32, 32, 3) в (1, 32, 32, 3)
+                # # отримання конфігурації додатка 'app_image'
+                # AppConfig = apps.get_app_config('app_image')
+                # # з отриманої конфігурації отримується модель
+                # model = AppConfig.model
+                # # передбачення класу зображення за допомогою переданої моделі
+                # model_inference = ModelInference(model)
+                # predicted_class = model_inference.predict_class(img_32x32_array)
 
-                    print(f'Розмірність масиву додавання додаткового виміру: {image_array.shape}')
-                else:
-                    print(f'Розмірність масиву дорівнює {image_array.shape}, підходяча розмірність - (32, 32, 2) або (32, 32, 4)')
-
-                # Класифікація---------------------------
-                image_array = image_array / 255.0
-                img_32x32_array = np.expand_dims(image_array, axis=0)  # перетворення (32, 32, 3) в (1, 32, 32, 3)
-                print(f'Розмірність кінцевого масиву: {img_32x32_array.shape}')
-
-                # отримання конфігурації додатка 'app_image'
-                AppConfig = apps.get_app_config('app_image')
-                # з отриманої конфігурації отримується модель
-                model = AppConfig.model
-
-                # передбачення класу зображення за допомогою переданої моделі
-                model_inference = ModelInference(model)
-                predicted_class = model_inference.predict_class(img_32x32_array)
-                # Класифікація---------------------------
+                # Класифікація
+                predicted_class = svg_classification(image_array, ModelInference)
 
 
-                # конвертуємо зображення в формат RGB для збереження без альфа-каналу (JPEG його не підтримує)
-                img_32x32_rgb = img_32x32.convert('RGB')
+                # # конвертуємо зображення в формат RGB для збереження без альфа-каналу (JPEG його не підтримує)
+                # img_32x32_rgb = img_32x32.convert('RGB')
+                #
+                # # задаємо ім'я тимчасового файлу
+                # temp_filename = 'temp_image.jpg'
+                #
+                # # зберігаємо перетворене зображення у файл формату JPEG
+                # img_32x32_rgb.save(temp_filename, format='JPEG')
+                #
+                # # завантажуємо зображення з файлу
+                # saved_image = PillowImage.open(temp_filename)
+                #
+                # # завантажуємо зображення в хмару та отримуємо його URL
+                # cloudinary_url = save_picture_to_claud(saved_image)
+                #
+                # # видаляємо тимчасовий файл
+                # os.remove(temp_filename)
+                #
+                # # Збереження URL зображення з Cloudinary у базу даних
+                # try:
+                #     image_instance = form.save(commit=False)
+                #     image_instance.cloudinary_image = cloudinary_url
+                #     image_instance.save()  # при цьому іде запис в БД і запис оригінального файлу на диск
+                # except Exception as e:
+                #     return HttpResponseServerError(f"Помилка при збереженні в БД: {str(e)}")
 
-                # # виведемо зображення на екран-----------------------воно ЧОРНЕ для чорно-білого зображення!
-                # plt.imshow(img_32x32_rgb)
-                # plt.show()
+                save_jpeg_and_url_from_svg(form, img_32x32)
 
-                # # Зберігаємо зображення-----------------------
-                # img_32x32_rgb.save('temp_filename.jpeg', format='JPEG')
-
-                # задаємо ім'я тимчасового файлу
-                temp_filename = 'temp_image.jpg'
-
-                # зберігаємо перетворене зображення у файл формату JPEG
-                img_32x32_rgb.save(temp_filename, format='JPEG')
-
-                # Отображаем изображение-----------------------
-
-
-                # завантажуємо зображення з файлу
-                saved_image = PillowImage.open(temp_filename)
-
-                # завантажуємо зображення в хмару та отримуємо його URL
-                cloudinary_url = save_picture_to_claud(saved_image)
-
-                # видаляємо тимчасовий файл
-                os.remove(temp_filename)
-
-                # Збереження URL зображення з Cloudinary у базу даних
-                try:
-                    image_instance = form.save(commit=False)
-                    image_instance.cloudinary_image = cloudinary_url
-                    image_instance.save()  # при цьому іде запис в БД і запис оригінального файлу на диск
-                except Exception as e:
-                    return HttpResponseServerError(f"Помилка при збереженні в БД: {str(e)}")
 
                 # видаляємо тимчасовий файл з диска
                 os.remove(uploaded_image.name)
 
-
             else:
                 # ---------------------------------------Растрові зображення (чорно-білі та кольорові)
-
-                # отримаємо зображення розміром 32х32 пікселі з оригінального зображення та відповідного масиву
+                # отримуємо зображення розміром 32х32 пікселі з оригінального зображення та відповідного масиву
                 img_32x32, img_32x32_array = preprocess_image(uploaded_image)
 
                 # отримання конфігурації додатка 'app_image'
@@ -166,9 +148,14 @@ def home(request):
                 # видаляємо тимчасовий файл з диска
                 os.remove(uploaded_image.name)
 
+    # return render(request,
+    #               template_name='app_image/index.html',
+    #               context={"form": form, "output_text": predicted_class})
+
     return render(request,
                   template_name='app_image/index.html',
                   context={"form": form, "output_text": predicted_class})
+
 
 
 def action(request):
