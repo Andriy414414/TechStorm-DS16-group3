11e7451fb134bcde681c793796949831aa0423ce
Evgenius87 <plotnikov.evgenij@ukr.net>
2024-02-20 14:46:03 +0200

    fix frontend

--- app_picture_classify/app_picture_classify/settings.py
+++ app_picture_classify/app_picture_classify/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&4m@xrazgq-4pjoyxq=mp_wm&z^w833uybg8pes&g=+%lj0xfj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'my.piclhub.pp.ua', 'piclhub.pp.ua']
 
