280dc6f28ddcf9b9eeecbcdc1493a65f8c7bea8a
alex <asmodians@hotmail.com>
2024-02-21 03:14:02 +0200

    otkat

--- app_picture_classify/app_image/urls.py
+++ app_picture_classify/app_image/urls.py
@@ -3,14 +3,9 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-
 app_name = 'app_image'
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('/home', views.home, name='home'),  # app_image:home
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('home/', views.home, name='home'),
 ]
-
-
-
--- app_picture_classify/app_image/views.py
+++ app_picture_classify/app_image/views.py
@@ -3,9 +3,14 @@ import cloudinary.uploader
 import cloudinary.api
 import os
 import numpy as np
+import io
 
-from .utils import svg_reshape_to_32x32x3, svg_classification, save_jpeg_and_url_from_svg, \
-    jpg_classification, save_jpeg_and_url_from_jpg_and_jpeg
+from .utils import (svg_reshape_to_32x32x3, 
+                    svg_classification, 
+                    save_jpeg_and_url_from_svg,
+                    jpg_classification, 
+                    save_jpeg_and_url_from_jpg_and_jpeg)
+
 from django.shortcuts import render, redirect
 from .forms import ImageForm
 from .models import ImageModel
@@ -14,17 +19,19 @@ from .utils import preprocess_image, remove_img_from_cloud, PUBLIC_ID
 from wand.image import Image as WandImage
 from wand.color import Color
 from PIL import Image as PillowImage
-import io
 
 img_public_id = None
 
 
 def destroy_original_image_from_cloud(func):
     """
-    Функція destroy_original_image_from_cloud — це декоратор,
-    який видалить оригінальне зображення з cloudinary після виконнання основної функції
+    The destroy_original_image_from_cloud function is a decorator that removes the original image from Cloudinary
+    after it has been uploaded. This function is used to prevent users from uploading multiple images with the same PUBLIC_ID,
+    which would result in an error.
+    
+    :param func: Pass the function that will be decorated
+    :return: A function that takes the request as a parameter
     """
-
     def inner(request, *args):
         # Получаем PUBLIC_ID из сессии
         if request.session.get('PUBLIC_ID'):
@@ -35,46 +42,72 @@ def destroy_original_image_from_cloud(func):
 
     return inner
 
-
 class ModelInference:
     def __init__(self, model):
         self.model = model
 
     def predict_class(self, img):
+        """
+        The predict_class function takes an image and returns a prediction of the class that the image belongs to.
+        The function uses a pre-trained model to make this prediction.
+        
+        :param self: Represent the instance of the class
+        :param img: Pass the image to be classified
+        :return: The name of the class with the highest probability
+        """
         class_names = ['літак', 'автомобіль', 'птах', 'кіт', 'олень', 'собака', 'жаба', 'кінь', 'корабель', 'вантажівка']
-
         prediction = self.model.predict(img)
         result = self.format_predictions(prediction, class_names)
         return result 
-    
+
     def format_predictions(self, predictions, class_names):
+        """
+        The format_predictions function takes in a list of predictions and class names,
+        and returns a formatted string with the top two classes and their probabilities.
+        
+        
+        :param self: Represent the instance of the class
+        :param predictions: Get the main and second classes
+        :param class_names: Get the class name from the index
+        :return: A string with the name of the main class,
+        """
         main_classes = np.argmax(predictions, axis=1)
         second_classes = np.argsort(-predictions, axis=1)[:, 1]
 
-        percentages_main = np.zeros(len(predictions))
-        percentages_second = np.zeros(len(predictions))
-        percentages_others = np.zeros(len(predictions))
+        sum_all = np.sum(predictions)
+        percentage_main = predictions[0][main_classes[0]] / sum_all
+        percentage_second = predictions[0][second_classes[0]] / sum_all
+        percentage_others = 1 - percentage_main - percentage_second
 
-        formatted_predictions = []
+        formatted_prediction = f"{class_names[main_classes[0]]} - {percentage_main*100:.2f}%\n"
+        formatted_prediction += f"{class_names[second_classes[0]]} - {percentage_second*100:.2f}%\n"
+        formatted_prediction += f"Інші класи - {percentage_others*100:.2f}%"
 
-        for i in range(len(predictions)):
-            sum_all = np.sum(predictions[i])
-            percentages_main[i] = predictions[i][main_classes[i]] / sum_all
-            percentages_second[i] = predictions[i][second_classes[i]] / sum_all
-            percentages_others[i] = 1 - percentages_main[i] - percentages_second[i]
-
-            formatted_predictions.append(f"{class_names[main_classes[i]]} - {percentages_main[i]*100:.2f}%, {class_names[second_classes[i]]} - {percentages_second[i]*100:.2f}%, Інші класи - {percentages_others[i]*100:.2f}%")
-
-        return ", ".join(formatted_predictions)
+        return formatted_prediction
 
 
 def home_page(request):
+    """
+    The home_page function is the view function for the home page of our app.
+    It renders a template called 'home_page.html' and returns it to the user.
+    
+    :param request: Get the request object from django
+    :return: An httpresponse object, which is a web page
+    """
     return render(request,
                   template_name='app_image/home_page.html')
 
 
 @destroy_original_image_from_cloud
 def home(request):
+    """
+    The home function is the main function of the app.
+    It handles all requests to the root URL, and renders a template with a form for uploading images.
+    If an image is uploaded, it will be preprocessed and classified by our model, then rendered in another template.
+    
+    :param request: Get the request object
+    :return: A rendered template
+    """
     form = ImageForm(instance=ImageModel())
     predicted_class = ''
     img_url = None
@@ -136,8 +169,8 @@ def home(request):
                 # збереження зображення в хмару, його url в базу даних
                 save_jpeg_and_url_from_jpg_and_jpeg(form, img_32x32, request.user)
 
-            # видаляємо тимчасовий файл з диска
-            os.remove(uploaded_image.name)
+                # видаляємо тимчасовий файл з диска
+                # os.remove(uploaded_image.name)
 
     return render(request,
                   template_name='app_image/index.html',
